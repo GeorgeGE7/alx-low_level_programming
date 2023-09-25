ad8b489836db64a26ad44fe91de10ea1b40706c3
George Malak <elgeo.e.ge.7@gmail.com>
2023-09-25 11:03:02 -0700

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements
- * @h: linked list of type listint_t
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t number = 0;
@@ -17,5 +16,6 @@ size_t print_listint(const listint_t *h)
 		number++;
 		h = h->next;
 	}
+
 	return (number);
 }
