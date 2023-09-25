4233e9fb37a90e97f6f3fbcf25ab1beead8c62b0
George <111939755+GeorgeGE7@users.noreply.github.com>
2023-09-25 21:47:51 +0300

    try to fix 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,25 +1,25 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
+ * reverse_listint - a linked list reverses
+ * @head: first node pointerr
  *
- * Return: pointer to the first node in the new list
+ * Return: first node pointer
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
+	listint_t *prevus = NULL;
 	listint_t *next = NULL;
 
 	while (*head)
 	{
 		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
+		(*head)->next = prevus;
+		prevus = *head;
 		*head = next;
 	}
 
-	*head = prev;
+	*head = prevus;
 
 	return (*head);
 }
