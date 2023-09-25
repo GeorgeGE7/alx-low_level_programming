c695f6479ad6e7519b2363f0a53ec7b433336390
George <111939755+GeorgeGE7@users.noreply.github.com>
2023-09-25 21:19:25 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * listint_len - returns length
+ * @h: listint_t linked list
  *
- * Return: number of nodes
+ * Return: nudes number ...
  */
 size_t listint_len(const listint_t *h)
 {
