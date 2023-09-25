c5231d9268d2033642f96d7f3f0706d6e302ab3c
George <111939755+GeorgeGE7@users.noreply.github.com>
2023-09-25 21:23:52 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * free_listint2  linked lest freeing
+ * @head: pointer to freed
  */
 void free_listint2(listint_t **head)
 {
