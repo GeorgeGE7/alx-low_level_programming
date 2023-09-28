fcabbf385da6aa95694fd01b8ff2974f1be8a235
George <111939755+GeorgeGE7@users.noreply.github.com>
2023-09-28 16:43:28 +0300

    Modify 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * get_bit - get the value of a bit at a given index
- * @n: number to evaluate
- * @index: index starting from 0, of the bit we want to get
- * Return: Value of bit at index, or -1 if error
+ * get_bit - bring the value of a bit at an index
+ * @n: number to gen
+ * @index: index begins from 0
+ * Return: Value of bit at index, or an error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int hold;
+	unsigned long int stay;
 
 	if (index > 64)
 		return (-1);
 
-	hold = n >> index;
+	stay = n >> index;
 
-	return (hold & 1);
+	return (stay & 1);
 }
