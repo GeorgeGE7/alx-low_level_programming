638ec18a2866e268f31ee43fb9a4d4fdcd135eab
George <111939755+GeorgeGE7@users.noreply.github.com>
2023-10-02 17:30:55 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,10 +13,10 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, length = 0;
 
-	if (filename === NULL)
+	if (filename == NULL)
 		return (-1);
 
-	if (text_content !== NULL)
+	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
 			length++;
